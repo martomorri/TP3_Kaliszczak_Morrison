065a3597c2cd03d4f400300d89bbce16f9dd1ea7
martomorri <106319796+martomorri@users.noreply.github.com>
2023-04-20 10:38:16 -0300

    falta ver detalle producto

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
 const getProducts = url => {
     axios.get(url)
     .then(res => {
-        const ulProductos = document.getElementById("productos");
+        const tablaProductos = document.getElementById("tablaProductos");
         const totalDiv = document.getElementById("totalDiv");
         totalDiv.innerHTML = `Total: ${res.data.total}`;
         res.data.products.forEach(element => {
-            const unLi = document.createElement("li");
-            unLi.innerHTML = `${element.title}`;
-            ulProductos.appendChild(unLi);
+            const fila = document.createElement("tr");
+            fila.setAttribute("id", `${element.id}`)
+            fila.innerHTML = `<th>${element.id}</th>
+                            <td>${element.title}</td>
+                            <td>U$D${element.price}</td>
+                            <td>${element.rating}</td>
+                            <td><button id="verMas" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalProducto">Ver Mas</button></td>`;
+            tablaProductos.appendChild(fila);
         });
-        console.log(res);
-    })
-}
 
-const filterProducts = text => {
-    let url = "https://dummyjson.com/products/search?q=" + text;
-    axios.get(url)
-    .then(res => {
         console.log(res);
-        const ulProductos = document.getElementById("productos");
-        ulProductos.innerHTML = "";
-        res.data.products.forEach(element => {
-            const unLi = document.createElement("li");
-            unLi.innerHTML = `${element.title}`;
-            ulProductos.appendChild(unLi);
-        });
     })
 }
 
 getProducts("https://dummyjson.com/products");
 
+const filterProducts = text => {
+    let url = "https://dummyjson.com/products/search?q=" + text;
+    axios.get(url)
+    .then(res => {
+        console.clear();
+        console.log(res);
+        const tablaProductos = document.getElementById("tablaProductos");
+        tablaProductos.innerHTML = "";
+        res.data.products.forEach(element => {
+            const fila = document.createElement("tr");
+            fila.setAttribute("id", `${element.id}`)
+            fila.innerHTML = `<th>${element.id}</th>
+                            <td>${element.title}</td>
+                            <td>U$D${element.price}</td>
+                            <td>${element.rating}</td>
+                            <td><button id="verMas" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalProducto">Ver Mas</button></td>`;
+            tablaProductos.appendChild(fila);
+        });
+    })
+}
+
 const search = document.getElementById("buscarBoton");
+const input = document.getElementById("buscar");
+search.setAttribute("disabled", "true");
+input.onkeyup = (e) => {
+    if (input.value) search.removeAttribute("disabled");
+    else search.setAttribute("disabled", "true");
+}
 search.onclick = (e) => {
-    const input = document.getElementById("buscar");
     filterProducts(input.value);
+}
+
+const more = document.getElementById("verMas");
+more.onclick = (e) => {
+    const titulo = document.getElementById("titulo");
+    const detalle = document.getElementById("producto");
 }
