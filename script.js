cf4ca629f48e837e9210f0e17ac5f15d6f32567f
martomorri <106319796+martomorri@users.noreply.github.com>
2023-04-27 10:41:52 -0300

    casi terminado, hay problema con el volver de la paginacion

--- script.js
+++ script.js
@@ -1,54 +1,89 @@
-const getProducts = url => {
+let skip = 0;
+const limit = 30;
+
+const getProducts = (url, skip) => {
+    url += skip;
+    console.log(url);
     axios.get(url)
-    .then(res => {
-        const tablaProductos = document.getElementById("tablaProductos");
-        const totalDiv = document.getElementById("totalDiv");
-        totalDiv.innerHTML = `Total: ${res.data.total}`;
-        res.data.products.forEach(element => {
-            const fila = document.createElement("tr");
-            //fila.setAttribute("id", `${element.id}`)
-            fila.innerHTML = `<th>${element.id}</th>
+        .then(res => {
+            const tablaProductos = document.getElementById("tablaProductos");
+            // const totalDiv = document.getElementById("totalDiv");
+            // totalDiv.innerHTML = `Total: ${res.data.total}`;
+            res.data.products.forEach(element => {
+                const fila = document.createElement("tr");
+                //fila.setAttribute("id", `${element.id}`)
+                fila.innerHTML = `<th>${element.id}</th>
                             <td>${element.title}</td>
                             <td>U$D${element.price}</td>
                             <td>${element.rating}</td>
-                            <td><button onclick="verMas(${element.id})" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalProducto">Ver Mas</button></td>`;
-            tablaProductos.appendChild(fila);
-        });
+                            <td><button onclick="verMas(${element.id})" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalProducto">Ver Mas</button></td>`;
+                tablaProductos.appendChild(fila);
+            });
 
-        console.log(res);
-    })
+            document.getElementById("masProductos").onclick = () => {
+                tablaProductos.innerHTML = "";
+                const paginacion = document.getElementById("paginacion");
+
+                console.log(volver);
+                if (!document.getElementById("volver"));
+                paginacion.appendChild(volver);
+                skip += limit;
+                console.log(res.data.skip)
+                getProducts("https://dummyjson.com/products?limit=" + limit + "&skip=", skip);
+            }
+            // console.log(res.data.skip);
+
+            console.log(res);
+        })
 }
 
-getProducts("https://dummyjson.com/products");
+const paginacion = document.getElementById("paginacion");
+const volver = document.createElement("a");
+volver.setAttribute("id", "volver");
+volver.innerHTML = "Volver";
+volver.onclick = () => {
+    tablaProductos.innerHTML = "";
+    skip -= limit;
+    if (skip == 0) {
+        paginacion.removeChild(volver);
+    }
+    getProducts("https://dummyjson.com/products?limit=" + limit + "&skip=", skip);
+}
+
+if (!document.getElementById("volver"))
+    paginacion.appendChild(volver);
+
+
+getProducts("https://dummyjson.com/products?limit=" + limit + "&skip=", 0);
 
 const filterProducts = text => {
-    console.log("hola");
+    // console.log("hola");
     let url = "https://dummyjson.com/products/search?q=" + text;
     axios.get(url)
-    .then(res => {
-        console.log(res);
-        const tablaProductos = document.getElementById("tablaProductos");
-        tablaProductos.innerHTML = "";
-        res.data.products.forEach(element => {
-            const fila = document.createElement("tr");
-            //fila.setAttribute("id", `${element.id}`)
-            fila.innerHTML = `<th>${element.id}</th>
+        .then(res => {
+            console.log(res);
+            const tablaProductos = document.getElementById("tablaProductos");
+            tablaProductos.innerHTML = "";
+            res.data.products.forEach(element => {
+                const fila = document.createElement("tr");
+                //fila.setAttribute("id", `${element.id}`)
+                fila.innerHTML = `<th>${element.id}</th>
                             <td>${element.title}</td>
                             <td>U$D${element.price}</td>
                             <td>${element.rating}</td>
-                            <td><button onclick="verMas(${element.id})" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalProducto">Ver Mas</button></td>`;
-            tablaProductos.appendChild(fila);
-        });
-    })
+                            <td><button onclick="verMas(${element.id})" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalProducto">Ver Mas</button></td>`;
+                tablaProductos.appendChild(fila);
+            });
+        })
 }
 
 const getById = id => {
     let url = "https://dummyjson.com/products/" + id;
     return axios.get(url)
-        /*.then(res => {
-            console.log(res.data);
-            return res.data;
-        })*/
+    /*.then(res => {
+        console.log(res.data);
+        return res.data;
+    })*/
 }
 
 const search = document.getElementById("buscarBoton");
@@ -73,10 +108,52 @@ const verMas = id => {
     const product = getById(id).then(product => {
         console.log(product);
         titulo.innerHTML = `${product.data.title}`;
-        detalle.innerHTML = `${product.data.description}\n
-                            Precio: ${product.data.price}\n
-                            Rating: ${product.data.rating}\n
-                            <img src="${product.data.images[0]} width="100%" height=auto>`;
+        detalle.innerHTML = `<p>${product.data.description}</p>
+                            <hr style="border: 1px solid black; padding:0; opacity: 100;">
+                            <b>Precio:</b>U$D ${product.data.price}<br>
+                            <b>Rating:</b> ${product.data.rating}✡<br>
+                            <img src="${product.data.thumbnail}" width="100%" height=auto>`;
     }
     );
+}
+
+const getCategories = () => {
+    let url = "https://dummyjson.com/products/categories";
+    axios.get(url)
+        .then(res => {
+            console.log(res);
+            const dropdown = document.getElementById("categories");
+            dropdown.innerHTML = "";
+            console.log(dropdown);
+            res.data.forEach(element => {
+                console.log(element);
+                const categorie = document.createElement("li");
+                categorie.innerHTML = `<a class="dropdown-item" href="#" onclick="filterByCategories('${element}')">${element}</a>`
+                // console.log(categorie);
+                dropdown.appendChild(categorie);
+            });
+        })
+}
+
+// getCategories("https://dummyjson.com/products/categories");
+
+const filterByCategories = cat => {
+    // console.log("hola");
+    let url = "https://dummyjson.com/products/category/" + cat;
+    axios.get(url)
+        .then(res => {
+            console.log(res);
+            const tablaProductos = document.getElementById("tablaProductos");
+            tablaProductos.innerHTML = "";
+            res.data.products.forEach(element => {
+                const fila = document.createElement("tr");
+                //fila.setAttribute("id", `${element.id}`)
+                fila.innerHTML = `<th>${element.id}</th>
+                            <td>${element.title}</td>
+                            <td>U$D${element.price}</td>
+                            <td>${element.rating}</td>
+                            <td><button onclick="verMas(${element.id})" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalProducto">Ver Mas</button></td>`;
+                tablaProductos.appendChild(fila);
+            });
+        })
 }
